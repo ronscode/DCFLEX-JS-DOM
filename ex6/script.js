e3962abf37d32e6f0734869bbddeae3dad6826dd
Ron Cagle <ron@roncagle.com>
2020-02-21 18:01:00 -0500

    Fixed increment counter

--- ex6/script.js
+++ ex6/script.js
@@ -7,14 +7,14 @@ var counter = document.getElementById('counter');
 var count = 0; // This variable holds the current value of the counter!
 
 incrementButton.addEventListener('click', function () {
-
-  counter.innerHTML = count++;
-
+  count += 1;
+  counter.innerHTML = count;
 });
 
 decrementButton.addEventListener('click', function () {
 
-  counter.innerHTML = count--;
+  --count;
+  counter.innerHTML = count;
 
 });
 
